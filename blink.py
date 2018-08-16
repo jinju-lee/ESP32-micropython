d8c1c7875ce950bc11c289810c0f09221cbfcbe8
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-16 16:20:43 +0900

    led blink

---
+++ blink.py
@@ -0,0 +1,9 @@
+import machine
+import utime
+
+pin21 = machine.Pin(21, machine.Pin.OUT)
+while True:
+        pin21.value(1)
+        utime.sleep_ms(500)
+        pin21.value(0)
+        utime.sleep_ms(500)
