7b7edad1ea99770f4553424d245c28d8c41792c9
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-23 15:39:03 +0900

    Update and rename uart.py to uart-wroom.py

--- uart-wroom.py
+++ uart-wroom.py
@@ -2,13 +2,21 @@ import utime
 from machine import UART
 
 u1_tx = UART(1, 400000)
+u1_rx = UART(1, 400000)
+u2_tx = UART(2, 400000)
 u2_rx = UART(2, 400000)
 
 u1_tx.init(400000, bits=8, parity=None, stop=1, tx=10)
+u1_rx.init(400000, bits=8, parity=None, stop=1, tx=9)
+u2_tx.init(400000, bits=8, parity=None, stop=1, tx=17)
 u2_rx.init(400000, bits=8, parity=None, stop=1, rx=16)
 
 while True:
 	u1_tx.write("u1_HELLO WORLD!!")
 	print(u2_rx.read())
+	u2_tx.write("u2_HELLO WORLD!!")
+	print(u1_rx.read())
 	utime.sleep(1)
+
+	
 	
